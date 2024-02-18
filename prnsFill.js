f70c0050c4a6512b1224f27ce4f7d7bcd4e53700
toranine09 <83958849+toranine09@users.noreply.github.com>
2024-02-18 17:07:17 +0000

    Update prnsFill.js
    Added ignore class/id filter. Updated variable names.

--- prnsFill.js
+++ prnsFill.js
@@ -1,37 +1,47 @@
-const prnsElementId = "prnshere";
-const prnsLink = "https://pronouns.cc/@toranine09";
-const prnsFlavourText = ["pronoun list", "view my pronouns"];
-const fallbackprnsFlavourText = "he/they/it";
-const textRandomType = "uniform";
-  // VALID OPTIONS:
-  // uniform = randomly selected on load but uniform across page
-  // trueRandom = every instance is random
+// user setup begins
+const prnsClassName = "prnshere"; // where to target
+const prnsFillWithHref = "https://pronouns.cc/@toranine09"; // link to fill with
+const prnsFlavourText = ["pronoun list", "view my pronouns", "he/they/it"];
+const prnsFallbackFlavourText = "he/they/it";
+const prnsTextRandomType = "uniform"; // how random/type of randomness
+const prnsFillIgnoreClass = "prnsfillignore"; // classname to skip
+const prnsFillIgnoreID = "prnsfillignore"; // id to skip
+// end of user setup
 
-if (textRandomType == "uniform" || textRandomType == "trueRandom") {
-  var prnsElements = document.getElementsByClassName(prnsElementId);
+  /* TODO:
+    - use apis for optional filler text
+      https://docs.pronouns.cc/api/ (?) needs user id
+      https://en.pronouns.page/api (?) cant fetch user data
+      pronouny.xyz (?) cant find
+  */
+
+if (prnsTextRandomType == "uniform" || prnsTextRandomType == "trueRandom") {
+  var prnsElements = document.getElementsByClassName(prnsClassName);
   if (prnsElements) {
-    if (textRandomType == "uniform") {
+    if (prnsTextRandomType == "uniform") {
       try {
         var randomText = prnsFlavourText[Math.floor(Math.random() * prnsFlavourText.length)];
       } catch (e) {
-        randomText = fallbackprnsFlavourText;
-        console.error("prnsFill random failed - " + e + " - set to fallback (" + fallbackprnsFlavourText + ")");
+        randomText = prnsFallbackFlavourText;
+        console.error("prnsFill random failed - " + e + " - set to fallback (" + prnsFallbackFlavourText + ")");
       }
     };
     for (var i = 0; i < prnsElements.length; i++) {
-      if (textRandomType == "trueRandom") {
-        try {
-          var randomText = prnsFlavourText[Math.floor(Math.random() * prnsFlavourText.length)];
-        } catch (e) {
-          randomText = fallbackprnsFlavourText;
-          console.error("prnsFill random failed - " + e + " - set to fallback (" + fallbackprnsFlavourText + ")");
-        }
+      if (!prnsElements[i].className.toString().match(prnsFillIgnoreClass) && !prnsElements[i].id.toString().match(prnsFillIgnoreID)) {
+        if (prnsTextRandomType == "trueRandom") {
+          try {
+            var randomText = prnsFlavourText[Math.floor(Math.random() * prnsFlavourText.length)];
+          } catch (e) {
+            randomText = prnsFallbackFlavourText;
+            console.error("prnsFill random failed - " + e + " - set to fallback (" + prnsFallbackFlavourText + ")");
+          }
+        };
+        prnsElements[i].href = prnsFillWithHref;
+        prnsElements[i].innerText = randomText;
+        prnsElements[i].style.display = "inline";
       };
-      prnsElements[i].href = prnsLink;
-      prnsElements[i].innerText = randomText;
-      prnsElements[i].style.display = "inline";
     };
   };
 } else {
-  console.error("prnsFill failed - invalid textRandomType (" + textRandomType + ") expected \"uniform\" or \"trueRandom\"");
+  console.error("prnsFill failed - invalid prnsTextRandomType, expected 'uniform' or 'trueRandom', got '"+ prnsTextRandomType + "'");
 };
